6b42e1331b857b7b2226a44c57573c6d444a1c6d
@johicute5 <irincojohnico@gmail.com>
2022-11-16 15:59:05 +0800

    Initial commit

---
+++ src/main/java/com/example/onlineclinic/OnlineClinicApplication.java
@@ -0,0 +1,13 @@
+package com.example.onlineclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/onlineclinic/OnlineClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.onlineclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
